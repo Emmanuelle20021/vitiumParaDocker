29083db77ebb0ca93c4da533d5568eaf60c4c1ae
Emma Mora <javieremma20021_@outlook.com>
2023-03-06 22:49:08 -0600

    Agregación de pantalla Splahs

--- lib/Pantallas/splahs.dart
+++ lib/Pantallas/splahs.dart
@@ -13,9 +13,23 @@ class _SplahsState extends State<Splahs> {
     return Scaffold(
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.network(
-                "https://drive.google.com/file/d/1MbktlIDWsOQpxSepG-DxGJOyUlWcRH4X/view?usp=sharing"),
+            FractionallySizedBox(
+              widthFactor: 0.4,
+              child: Image.network(
+                  "https://i.postimg.cc/vB0ZCv0X/Logo-Circular-Splahs.png"),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 15),
+              child: Text(
+                "Vitium, conectando con las oportunidades",
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontSize: MediaQuery.of(context).size.shortestSide * 0.05,
+                ),
+              ),
+            ),
           ],
         ),
       ),
