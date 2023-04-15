2cbb8f3de9591d1493f86cf8b51996e9e8713f9d
MichelleVaughan0 <mvaughan.00@hotmail.com>
2023-04-14 18:57:18 -0600

    Actualizar clase vacante

--- lib/Funcionalidades/vacante.dart
+++ lib/Funcionalidades/vacante.dart
@@ -1,1 +1,11 @@
-class Vacante {}
+class Vacante {
+  int id = 0;
+  String nombreVacante = '';
+  String descripcion = '';
+  String salario = '';
+  String tipoDeEmpleo = '';
+  String ubicacion = '';
+  String responsabilidades = '';
+  bool estado = true;
+  List<String> discapacidadesPermitidas = [];
+}
