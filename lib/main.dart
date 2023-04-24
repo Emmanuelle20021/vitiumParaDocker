b933773042bc4179f6f679dc24546b753f2b8bdc
MichelleVaughan0 <mvaughan.00@hotmail.com>
2023-04-24 16:45:51 -0600

    Agregar fuente assets

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:vitium_app/pantallas/usuario/home/home_user.dart';
 import 'package:vitium_app/pantallas/usuario/login/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:vitium_app/pantallas/splash/splahs.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,14 +33,19 @@ class VitiumApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+       theme: ThemeData(
+            colorScheme: ColorScheme.fromSwatch().copyWith(
+                primary: const Color(0xff254AB0),
+                secondary: const Color.fromARGB(255, 0, 153, 31)));)}
+                
+
+   /*    debugShowCheckedModeBanner: false,
       useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
+      home:
+            const //SplahsScreens() /*StreamBuilder<User?>(
+        //stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             return const HomeUser();
@@ -47,6 +54,6 @@ class VitiumApp extends StatelessWidget {
           }
         },
       ),
-    );
-  }
-}
+    );*/
+  
+  */
