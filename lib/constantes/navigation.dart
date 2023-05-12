fc82944be2c96593a6cb4246a438da504923ff2c
Emma Mora <javieremma20021_@outlook.com>
2023-05-12 17:50:09 -0600

    Agragación de la pantalla de vacantes.

--- lib/constantes/navigation.dart
+++ lib/constantes/navigation.dart
@@ -61,3 +61,5 @@ var menu = const BottomNavigationBarItem(
   activeIcon: Icon(Icons.home_filled),
   label: 'Home',
 );
+
+
--- lib/pantallas/usuario/home/home_user.dart
+++ lib/pantallas/usuario/home/home_user.dart
@@ -1,13 +1,10 @@
-import 'dart:math';
-import 'package:auto_size_text/auto_size_text.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:vitium_app/constantes/constantes.dart';
-import 'package:vitium_app/constantes/navigation.dart';
-import 'package:vitium_app/pantallas/splash/splahs.dart';
+import 'package:vitium_app/pantallas/usuario/home/home_screen.dart';
+import 'package:vitium_app/pantallas/usuario/home/perfil_screen.dart';
+import 'package:vitium_app/pantallas/usuario/home/trabajos_screen.dart';
 
 class HomeUsuario extends StatefulWidget {
   const HomeUsuario({super.key});
@@ -16,367 +13,49 @@ class HomeUsuario extends StatefulWidget {
   State<HomeUsuario> createState() => _HomeUsuarioState();
 }
 
-late double anchoGlobal;
-late IconData icono;
-var refreshKey = GlobalKey<RefreshIndicatorState>();
-FirebaseFirestore db = FirebaseFirestore.instance;
-List<FichaEmpresa> empresas = List.empty(growable: true);
-String busqueda = "";
-
 final User? user = FirebaseAuth.instance.currentUser;
 
 class _HomeUsuarioState extends State<HomeUsuario> {
-  @override
-  void initState() {
-    rellenarEmpresas("");
-    super.initState();
-    FlutterNativeSplash.remove();
-    buscarVacante();
-    icono = iconos[Random().nextInt(6 - 0)];
-  }
-
-  void setGlobal(ancho) {
-    anchoGlobal = ancho;
-  }
-
-  QueryDocumentSnapshot<Object>? vacante;
-
-  buscarVacante() async {
-    icono = iconos[Random().nextInt(6 - 0)];
-    vacante = null;
-    QueryDocumentSnapshot<Object>? vacantes;
-    await db.collectionGroup("Vacantes").get().then(
-          (value) => {
-            value.docs.toList().forEach((element) {
-              if (vacante == null) {
-                vacantes = element;
-              }
-            })
-          },
-        );
-    setState(() {
-      vacante ??= vacantes;
-    });
-  }
-
-  Future<Type> rellenarEmpresas(busqueda) async {
-    refreshKey.currentState?.show();
-    List<FichaEmpresa> empresa = List.empty(growable: true);
-    await db.collectionGroup("Empresa").get().then(
-          (QuerySnapshot querySnapshot) => {
-            querySnapshot.docs.toList().forEach(
-              (element) {
-                String nombre = element.get("Nombre");
-                if (nombre.contains(busqueda)) {
-                  FichaEmpresa fichaEmpresa = FichaEmpresa(
-                    element.get("Imagen"),
-                    element.get("Nombre"),
-                  );
-                  empresa.add(fichaEmpresa);
-                }
-              },
-            ),
-          },
-        );
-    setState(() {
-      empresas = List.empty(growable: true);
-      empresas = empresa;
-    });
-    return Null;
-  }
-
+  var _index = 0;
+  var screens = [
+    const HomeScreen(),
+    const TrabajosScreen(),
+    const PerfilScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
-    double ancho = MediaQuery.of(context).size.width;
-    double alto = MediaQuery.of(context).size.height;
-    setGlobal(ancho);
-    TextEditingController busquedaController = TextEditingController();
     return Scaffold(
-      bottomNavigationBar: const Barra(),
-      body: RefreshIndicator(
-        key: refreshKey,
-        onRefresh: () => rellenarEmpresas(busqueda),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: alto * 0.05,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Flexible(
-                  fit: FlexFit.loose,
-                  flex: 2,
-                  child: AutoSizeText.rich(
-                    textAlign: TextAlign.justify,
-                    maxFontSize: 33,
-                    minFontSize: 30,
-                    maxLines: 2,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                    TextSpan(
-                      children: [
-                        const TextSpan(
-                          text: "Descubre tu \n",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        TextSpan(
-                          text: "Trabajo favorito",
-                          style: TextStyle(color: accent),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                CircleAvatar(
-                  radius: 25,
-                  backgroundImage: AssetImage(logoVitium),
-                  backgroundColor: Colors.transparent,
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Flexible(
-                    fit: FlexFit.loose,
-                    flex: 2,
-                    child: TextField(
-                      onSubmitted: (value) {
-                        busqueda = value;
-                        rellenarEmpresas(busqueda);
-                      },
-                      inputFormatters: [
-                        FilteringTextInputFormatter.singleLineFormatter
-                      ],
-                      textCapitalization: TextCapitalization.words,
-                      textAlignVertical: TextAlignVertical.center,
-                      decoration: const InputDecoration(
-                        constraints: BoxConstraints.tightFor(height: 47),
-                        prefixIcon: Icon(Icons.search),
-                        hintText: "Buscar",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(10),
-                          ),
-                          borderSide: BorderSide(color: Colors.grey),
-                        ),
-                      ),
-                      controller: busquedaController,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                        color: accent,
-                      ),
-                      child: IconButton(
-                        color: background,
-                        splashRadius: 20,
-                        onPressed: () {},
-                        icon: const Icon(Icons.format_list_bulleted_sharp),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(left: 20, bottom: 10),
-              alignment: Alignment.centerLeft,
-              child: const AutoSizeText(
-                "Nuestras Empresas",
-                minFontSize: 15,
-                maxFontSize: 25,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: alto * 0.45,
-              child: Flexible(
-                fit: FlexFit.tight,
-                flex: 1,
-                child: compruebaEmpresas(empresas, alto, ancho),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              alignment: Alignment.centerLeft,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const AutoSizeText(
-                    "Más recientes",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                  TextButton(
-                    style: const ButtonStyle(
-                      splashFactory: NoSplash.splashFactory,
-                    ),
-                    onPressed: () {},
-                    child: const AutoSizeText("Ver todos"),
-                  ),
-                ],
-              ),
-            ),
-            vacanteFicha(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget compruebaEmpresas(empresas, alto, ancho) => empresas != Null
-      ? GestureDetector(
-          onTap: () {
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: GNav(
+          onTabChange: (value) {
             setState(() {
-              Navigator.pushReplacement(
-                  context,
-                  PageRouteBuilder(
-                    pageBuilder: (context, __, _) => const SplahsScreens(),
-                  ));
+              _index = value;
             });
           },
-          child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2),
             padding: const EdgeInsets.all(20),
-            scrollDirection: Axis.horizontal,
-            shrinkWrap: true,
-            itemCount: empresas.length,
-            itemBuilder: (context, index) {
-              return Container(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                width: ancho * 0.3,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      width: ancho * 0.3,
-                      decoration: BoxDecoration(
-                        color: background,
-                        border: Border.all(color: Colors.black),
-                        borderRadius: const BorderRadius.vertical(
-                          top: Radius.circular(10),
-                        ),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 20,
-                            offset: Offset(8, 7),
-                            blurStyle: BlurStyle.inner,
-                          ),
-                        ],
-                      ),
-                      height: alto * 0.1,
-                      child: Image.asset(
-                        logoVitium,
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black),
-                        borderRadius: const BorderRadius.vertical(
-                          bottom: Radius.circular(10),
-                        ),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 20,
-                            offset: Offset(8, 7),
-                            blurStyle: BlurStyle.inner,
-                          ),
-                        ],
-                        color: accent,
-                      ),
-                      height: alto * 0.05,
-                      width: ancho * 0.3,
-                      alignment: Alignment.center,
-                      child: Text(
-                        empresas[index]._nombre,
-                        style: TextStyle(color: background),
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-        )
-      : const Center(
-          child: Text("No hay empresas para mostrar"),
-        );
-
-  Widget vacanteFicha() => vacante != null
-      ? GestureDetector(
-          onTap: () {},
-          child: Container(
-            margin: const EdgeInsets.all(10),
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-            decoration: const ShapeDecoration(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20),
-                ),
-                side: BorderSide(width: .3, color: Colors.grey),
-              ),
+          activeColor: accent,
+          tabs: const [
+            GButton(
+              text: 'Inicio',
+              icon: Icons.home_filled,
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Icon(icono, color: primary, weight: anchoGlobal * .17),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(vacante?.get("Puesto"),
-                            textAlign: TextAlign.start),
-                        Text(
-                          vacante?.get("Empresa"),
-                          textAlign: TextAlign.start,
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(vacante?.get("Discapacidad")),
-                    const Spacer(),
-                    Text(vacante?.get("Salario")),
-                  ],
-                )
-              ],
+            GButton(
+              text: 'Trabajos',
+              icon: Icons.work,
             ),
-          ),
-        )
-      : const Center(child: Text("No hay vacantes"));
+            GButton(
+              text: 'Perfil',
+              icon: Icons.person,
+            ),
+          ],
+          color: tertiary,
+          gap: 8,
+        ),
+      ),
+      body: screens[_index],
+    );
+  }
 }
 
 class FichaEmpresa {
